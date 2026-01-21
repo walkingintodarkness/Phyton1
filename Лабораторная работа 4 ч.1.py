c8605cd49a0ee563fda682bbcfdb04b1ca7e586c
walkingintodarkness <koshkin_f4@mail.ru>
2026-01-21 19:14:46 +0300

    Add files via upload
    Добрый вечер! Прошу прощения за позднюю загрузку.

---
+++ Лабораторная работа 4 ч.1.py
@@ -0,0 +1,11 @@
+import json
+# TODO решите задачу
+def task() -> float:
+    with open('input.json', 'r') as f:
+        data = json.load(f)
+    total = 0
+    for item in data:
+        total += item['score'] * item['weight']
+    return round(total, 3)
+
+print(task())
